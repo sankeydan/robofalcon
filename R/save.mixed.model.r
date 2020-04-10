df99cb62ad9644a38741404ec5f0281e7cc00469
Daniel Sankey <dwsankey@gmail.com>
2020-04-10 15:18:37 +0100

    Commit_2020_04_10

--- R/save.mixed.model.r
+++ R/save.mixed.model.r
@@ -8,7 +8,7 @@ save.mixed.model = function( model , data, filename){
   CIs = as.data.frame ( intervals(model, which = "fixed")$fixed[,c(1,3)])
   names( CIs) = c("CI-2.5%", "CI-97.5%")
   t = cbind( t, CIs, cohens.d)
-  t = t[,c("Value","CI-2.5%", "CI-97.5%", "t-value", "cohens.d", "p-value")]
+  t = t[,c("Value","CI-2.5%", "CI-97.5%", "DF", "t-value", "cohens.d", "p-value")]
   
   write.csv( t , file = file.path (PROJHOME , "Output", "Statistics" , "Tables", filename))
   save ( model , file = file.path (PROJHOME , "Output", "Statistics" , "Models", filename))
--- source/source.r
+++ source/source.r
@@ -1,44 +1,7 @@
 
 load(file.path ( PROJHOME , "Output", "FinalDataSet", "ALLrobofalcon-data.rda"))
 load(file.path ( PROJHOME , "Output", "FinalDataSet", "REMrobofalcon-data.rda"))
-vars = c( "sqrt_diff_head",
-          "diff_head",
-          "diff_head_sharp",
-          "condition",
-          "cross.wind",
-          "dist2cent",
-          "dist2home",
-          "turn2falchead",
-          "dir.rel.ali",
-          "unique.flight",
-          "turn2falcpos",
-          "align",
-          "attract",
-          "small.big",
-          "turn2home",
-          "pigeon",
-          "Date",
-          "group.num")
-vars.nofalc = c( "sqrt_diff_head",
-                 "diff_head",
-                 "diff_head_sharp",
-                 "condition",
-                 "cross.wind",
-                 "dist2cent",
-                 "dist2home",
-                 "dir.rel.ali",
-                 "unique.flight",
-                 "align",
-                 "attract",
-                 "centrip",
-                 "support.wind",
-                 "speed",
-                 "small.big",
-                 "dist2site",
-                 "turn2home",
-                 "pigeon",
-                 "Date",
-                 "group.num")
+
 # samples
 sam9 = seq( 1,nrow(dat), 9 )
 sam24 = seq( 1,nrow(dat), 24 )
